eb0d205af227578fe5d20eadc7476c18438fa74d
Yoav Schwammenthal <yoavsc0302@gmail.com>
2020-12-15 18:56:14 +0200

    Added the function 'distribution_of_genotypes_plots', that displays plots of genotypes distirbutions for train, val and test data files

--- yoavmodule.py
+++ yoavmodule.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 inp = ['data/train_data.csv', 'data/val_data.csv', 'data/test_data.csv']
 
 
 def check_intercepted_data(list_of_files):
-    """Checks if the training, validating and testing data files overlap each other.
-
-        inp = list containing the names of the data files."""
+    """ Checks if the training, validating and testing data files overlap
+    each other, and prints the results :param list_of_files: list containing
+    the names of the data files
+    """
 
     interception_results = 'Interception results:'
     num_of_overlaps = 0
 
-    # for each file, reads the 'loc string' column values and inserts them into a numpy Array
+    # for each file, reads the 'loc string' column values and inserts them
+    # into a numpy Array
     train_array = pd.read_csv(list_of_files[0])['loc_string'].to_numpy()
     val_array = pd.read_csv(list_of_files[1])['loc_string'].to_numpy()
     test_array = pd.read_csv(list_of_files[2])['loc_string'].to_numpy()
@@ -34,9 +37,11 @@ def check_intercepted_data(list_of_files):
 
 
 def is_variation_length_ok(list_of_files):
-    """Checks if all the genetic variation lengths of each data file equals 1
-
-    inp = list containing the names of the data files."""
+    """
+    Checks if all the genetic variation lengths of each data file equals 1
+    :param list_of_files: list containing the names of the data files
+    :return: bool: True if all lengths are 1
+    """
 
     # Read train, valid and test data
     train_data = pd.read_csv(list_of_files[0])
@@ -46,15 +51,58 @@ def is_variation_length_ok(list_of_files):
     # Concat all data files into a dataframe 'all_data'
     all_data = pd.concat([train_data, val_data, test_data])
 
-    # Create an array out of 'start' and 'end' columns of 'all data' dataframe
-    start_arr = all_data['start'].to_numpy()
-    end_arr = all_data['end'].to_numpy()
+    # Create a series of length of each genetic variation
+    length_series = all_data['end'] - all_data['start']
 
-    # Create an array of length of each genetic variation
-    length_arr = end_arr-start_arr
+    # Return True if all lengths are 1, Return False if any of the lengths is
+    # different from 1
+    return all(length_series == 1)
 
-    # Return True if all lengths are 1, Return False if one of the lengths is different from 1
-    for len in length_arr:
-        if len != 1:
-            return False
-    return True
+
+def distribution_of_genotypes_plots(list_of_files):
+    """
+    For each data file(train, val and test) prints normalized bar plots of
+    genotype distribution, for father, mother and child.
+    :param list_of_files: list containing the names of the data files.
+    """
+
+    # Create dataframes out of train, val and test data csv files
+    train = pd.read_csv(list_of_files[0])
+    val = pd.read_csv(list_of_files[1])
+    test = pd.read_csv(list_of_files[2])
+
+    def plot_genotype_distribution(title, df):
+        """
+        Prints normalized bar plots of genotype distribution.
+        :param title: will be set as the title of the plot (string)
+        :param df: the dataframe (train, val, test)
+        """
+        # From data file: get father's mother's and child's genotypes
+        # distribution
+        distribution_father = df['H12148W'].value_counts().sort_index()
+        distribution_mother = df['M12148W'].value_counts().sort_index()
+        distribution_child = df['label'].value_counts().sort_index()
+
+        # Concat the dataframes genotype distribution of: father, mother and
+        # child into a single dataframe
+        dist_concat = pd.concat([distribution_father,
+                                 distribution_mother,
+                                 distribution_child], axis=1)
+
+        # Convert counts into percentage out of all samples, and bar plot the
+        # dataframe
+        ax = (dist_concat / dist_concat.sum()[0]).plot(kind='bar',
+                                                       title='Distribution of '
+                                                             'Genotypes - ' +
+                                                             title)
+
+        # Set y ticks as percentage instead of a simple number
+        vals = ax.get_yticks()
+        ax.set_yticklabels([f'{x:,.0%}' for x in vals])
+
+    plot_genotype_distribution('Train data', train)
+    plot_genotype_distribution('Val data', val)
+    plot_genotype_distribution('Test data', test)
+    plt.show()
+
+
